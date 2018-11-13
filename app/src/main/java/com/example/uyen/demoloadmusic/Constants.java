703518300c9472ba8e16c401c23e8f572e59307c
uyen <honguyen2996@gmail.com>
2018-11-13 08:35:24 +0700

    load music

---
+++ app/src/main/java/com/example/uyen/demoloadmusic/Constants.java
@@ -0,0 +1,9 @@
+package com.example.uyen.demoloadmusic;
+
+public class Constants {
+    public static final String NAME_SONG_1 = "Vô tình";
+    public static final String NAME_SONG_2 = "Chấp nhận";
+    public static final String NAME_SONG_3 = "Chuyện tình tôi";
+    public static final String NAME_SONG_4 = "HongKong1";
+    public static final String NOTIFICATION_TITLE = "Play music";
+}
